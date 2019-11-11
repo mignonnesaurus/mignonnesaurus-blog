d15cd7a394c0bc093a2d963fae6fff416cbda724
mignonnesaurus <eli@mignonnesaurus.com>
2019-11-11 23:29:04 +0100

    Adds the first version of the tour

---
+++ open_screenshots.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cd tour/screenshots
+for i in *.png; do open "$i"; done
---
+++ tour/blog_tour.py
@@ -0,0 +1,50 @@
+from seleniumbase import BaseCase
+import os
+
+MY_BLOG_USERNAME = os.getenv('MY_BLOG_USERNAME')
+MY_BLOG_PASSWORD = os.getenv('MY_BLOG_PASSWORD')
+
+
+class BlogTour(BaseCase):
+    def test_blog(self):
+        self.open(self.start_page)
+
+        home_page = self.get_current_url()
+        self.open(f'{home_page}admin/')
+
+        self.type('#id_username', MY_BLOG_USERNAME)
+        self.type('#id_password', MY_BLOG_USERNAME)
+        self.save_screenshot('login_page.png', folder='tour/screenshots')
+        self.click('input[type="submit"]')
+        self.save_screenshot('admin_page.png', folder='tour/screenshots')
+
+        self.click('link=VIEW SITE')
+
+        self.click('.glyphicon-plus')
+        self.save_screenshot('add_new_post.png', folder='tour/screenshots')
+        self.type('#id_title', 'Star Wars IX: The Rise of Skywalker')
+        self.type('#id_text', 'Star Wars IX: The Rise of Skywalker will be released on December 18, 2019. ')
+
+        self.click('button[type="submit"].save')
+
+        self.click('.publish')
+        self.open(home_page)
+        self.save_screenshot('posts_list.png', folder='tour/screenshots')
+
+        self.click('link=Star Wars IX: The Rise of Skywalker')
+        self.click('.add-comment')
+        self.save_screenshot('add_comment.png', folder='tour/screenshots')
+        self.type('#id_author', 'Yoda')
+        self.type('#id_text', 'Happy I am ❤️')
+        self.click('button[type="submit"].save')
+
+        self.click('.add-comment')
+        self.type('#id_author', 'Han Solo')
+        self.type('#id_text', 'Am I invited? 💔')
+        self.click('button[type="submit"].save')
+
+        self.save_screenshot('posts_list_with_comments.png', folder='tour/screenshots')
+
+        self.click('.glyphicon-remove')
+
+        self.click('link=Log out')
